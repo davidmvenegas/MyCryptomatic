3bd1774b36754b824505de431bff7f4205349481
david_venegas <venegasdavidm@gmail.com>
2021-11-13 15:39:27 -0500

    front page kinda done

--- src/0-home/Home.js
+++ src/0-home/Home.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import './home.css';
 
 function Home() {
     return (
         <div className="home-page">
             <div className="home-container">
-                <h1 className="home-header">CRYPTOMATIC</h1>
-                <h3 className="home-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, praesentium!</h3>
-                <button className="home-button">GO</button>
-            </div>
+                <div className="home-text-container">
+                    <h1 className="home-header">CRYPTOMATIC</h1>
+                    <h3 className="home-description">Track the Markets - Monitor your Portfolio - Trade Automaticly</h3>
+                    <button className="home-button">GO</button>
+                </div>
+		        <ul class="floating-coins">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+		        </ul>
+	        </div>
         </div>
     )
 }
