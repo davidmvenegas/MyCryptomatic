267e0f2e75208f416a8ea524a17a4873d4dfbc39
david_venegas <venegasdavidm@gmail.com>
2021-11-16 14:11:11 -0500

    david tuesday afternoon

--- src/2-portfolio/portfolio-items/Watchlist.js
+++ src/2-portfolio/portfolio-items/Watchlist.js
@@ -3,18 +3,36 @@ import "./watchlist.css"
 import WatchlistItem from './WatchlistItem'
 
 function Watchlist() {
+
     return (
         <div>
             <div className="watchlist-container">
                 <div className="watchlist">
                     <div className="watchlist-top">
-                        <h2>Items in Watchlist: 0</h2>
-                        <h2>Sort By</h2>
-                        <h3>Rank</h3>
-                        <h3>Name</h3>
-                        <h3>% Change</h3>
+                        <h2 className="watchlist-top-count-title">Items in Watchlist: <span className="watchlist-top-count">0</span></h2>
+                        <div className="watchlist-sort-container">
+                            <h2 className="watchlist-sort-title">Sort By:</h2>
+                            <form className="watchlist-sort-subContainer">
+                                <div className="watchlist-filter">
+                                    <input type="radio" name="sort-watchlist" id="sort-watchlist-name" defaultChecked />
+                                    <label className="watchlist-sort-item" htmlFor="sort-watchlist-name">Name</label>
+                                </div>
+                                <div className="watchlist-filter">
+                                    <input type="radio" name="sort-watchlist" id="sort-watchlist-rank" />
+                                    <label className="watchlist-sort-item" htmlFor="sort-watchlist-rank"><span>#</span>Rank</label>
+                                </div>
+                                <div className="watchlist-filter">
+                                    <input type="radio" name="sort-watchlist" id="sort-watchlist-change" />
+                                    <label className="watchlist-sort-item" htmlFor="sort-watchlist-change"><span>%</span>Change</label>
+                                </div>
+                            </form>
+                        </div>
                     </div>
+                    <div className="watchlist-seperator"></div>
                     <div className="watchlist-contents">
+                        <WatchlistItem/>
+                        <WatchlistItem/>
+                        <WatchlistItem/>
                         <WatchlistItem/>
                     </div>
                 </div>
--- src/header|footer/Header.js
+++ src/header|footer/Header.js
@@ -8,7 +8,7 @@ function Header() {
     return (
         <header>
             <div className="header-container">
-                <NavLink exact to="/"><img className="header-logo-img" src="./link-icon.png" alt="cryptomatic_logo"/><h1 className="header-logo">Cryptomatic</h1></NavLink>
+                <NavLink exact to="/"><img className="header-logo-img" src="https://cdn.logojoy.com/wp-content/uploads/20200305144650/Bitcoin-icon.png" alt="cryptomatic_logo"/><h1 className="header-logo">Cryptomatic</h1></NavLink>
                 <ul className="header-menu">
                     <li className="header-item"><NavLink to="/markets" activeClassName="active" className="header-link"><h1>Markets</h1></NavLink></li>
                     <li className="header-item"><NavLink to="/portfolio" activeClassName="active" className="header-link"><h1>Portfolio</h1></NavLink></li>
