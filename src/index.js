9a1c4cdcda7c1715ea5d02a9dc00b6d919d3be4d
david_venegas <venegasdavidm@gmail.com>
2022-01-30 14:21:13 -0500

    npm packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter } from "react-router-dom"
+import './index.css'
+import App from './App'
+import HttpsRedirect from 'react-https-redirect'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
-      <App />
-    </HashRouter>
+    <HttpsRedirect>
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
+    </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
